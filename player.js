4232edd8aba9efd0dbeff07b0910ce642587de78
Marco <patrickstar1281@yahoo.com>
2023-07-08 14:37:09 -0500

    set up simple jumping with space

--- player.js
+++ player.js
@@ -5,13 +5,30 @@ export class Player {
         this.height = 70;
         this.x = 20;
         this.y = 230;
+        this.vy = 0;
+        this.weight = 1;
         this.image = player;
     }
 
-    update(input) {
+    update(input) { 
+        // jumping movement 
         if (input.includes(' ')) {
-            this.y--;
-        }
+            this.vy -= 5;
+        };
+
+        this.y += this.vy;
+
+        if (!this.hitGround()) {
+            this.vy += this.weight;
+        } else {
+            // also add end game function right here
+            this.vy = 0;
+        };
+        
+        // top boundaries 
+        if (this.y < 0) {
+            this.y = 0;
+        };
     };
 
     draw(context) {
@@ -19,5 +36,9 @@ export class Player {
         context.fillRect(this.x, this.y, this.width, this.height);
         // add these addtional arguments later sx, sy, sw, sh
         context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    };
+
+    hitGround() {
+        return this.y >= this.game.height - this.height;
     };
 };
